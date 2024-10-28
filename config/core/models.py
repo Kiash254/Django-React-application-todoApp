3a2be3dfbbef53b945e140d0849a850f37572104
Kiash254 <codewithkiash@gmail.com>
2024-10-28 10:27:07 +0300

    added new data

--- config/core/models.py
+++ config/core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+# models.py
+
+from django.db import models
+
+class Blog(models.Model):
+    image = models.ImageField(upload_to='blog_images/')
+    name = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    posted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
